7c25ece6b0c5368d2d8c67c607390639524fbf01
Diego Campos <diecamdia@hotmail.es>
2015-12-01 16:36:51 +0100

    Implement INotifyPropertyChanged HW #1

---
+++ RestaurantManager.Models/OrderDataManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class OrderDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            this.MenuItems = base.Repository.StandardMenuItems;
+
+            this.CurrentlySelectedMenuItems = new List<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+        }
+        List<MenuItem> menuItems;
+
+        public List<MenuItem> MenuItems
+        {
+            get { return menuItems; }
+            set
+            {
+                menuItems = value;
+                this.OnPropertyChanged("MenuItems");
+            }
+        }
+
+        List<MenuItem> currentlySelectedMenuItems;
+
+        public List<MenuItem> CurrentlySelectedMenuItems
+        {
+            get { return currentlySelectedMenuItems; }
+            set
+            {
+                currentlySelectedMenuItems = value;
+                this.OnPropertyChanged("CurrentlySelectedMenuItems");
+            }
+        }
+    }
+}
